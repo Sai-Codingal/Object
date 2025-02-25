7111c9a9858641e60aca7e41149847281b164247
Sai-Codingal <saiiscool56@gmail.com>
2025-02-25 16:14:06 -0500

    Objects

---
+++ index.js
@@ -0,0 +1,5 @@
+var student={name:"Sai",age:13,Grade:8,fav_color:"Red"};
+document.getElementById("a1").innerHTML=student.name;
+document.getElementById("a2").innerHTML=student["age"];
+document.getElementById("a3").innerHTML=student.fav_color;
+document.getElementById("a4").innerHTML=Math.random();
